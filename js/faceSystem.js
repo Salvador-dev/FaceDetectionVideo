040dc725973721c967c233d5f511ccde486ca681
Salvador-dev <83884209+Salvador-dev@users.noreply.github.com>
2022-11-27 10:52:00 -0400

    Update faceSystem.js

--- js/faceSystem.js
+++ js/faceSystem.js
@@ -1,12 +1,4 @@
-const video = document.getElementById('video')
-
-Promise.all([
-    faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
-    faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
-    faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
-    faceapi.nets.faceExpressionNet.loadFromUri('./models')
-  ]).then(startVideo)
-  
+const video = document.getElementById('video') 
 
 function startVideo() {
     navigator.getUserMedia = (navigator.getUserMedia ||
